c0a5c10f97f99f4fd93ef12da718d8a8ca334d9f
Abby Cloft <acloft@uci.edu>
2018-05-08 14:51:03 -0700

    also saves to mongo db

--- server.js
+++ server.js
@@ -3,44 +3,75 @@ const app = express();
 const cheerio = require("cheerio");
 const rp = require("request-promise");
 const bodyParser = require("body-parser");
+const mongo = require("./mongodb");
+const conn = mongo.connection;
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const port = 9090;
 app.use(bodyParser.json());
 
-app.post("/", getIngredientsFrom);
+app.post("/", saveIngredientsFrom);
 
-app.listen(9090);
+mongo
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(port);
+  })
+  .then(() => {
+    console.log(`Magic happens on port ${port}`);
+  });
 
-function getIngredientsFrom(req, res) {
+function saveIngredientsFrom(req, res) {
   const options = {
     uri: req.body.recipe,
     transform: function(body) {
       return cheerio.load(body);
     }
   };
-
   rp(options)
     .then($ => {
-      let ingredients = {};
+      const recipe = {
+        recipeName: $("[data-serverid='MoreTitleURL']").text(),
+        recipeUrl: req.body.recipe,
+        ingredients: {}
+      };
+
       $("#g_ingredients .ingredient-group").each(function(index, element) {
         let sectionName = $(this)
           .find("[data-serverid='IngredientHeading']")
           .text()
-          ? $(this)
+          ? `${$(this)
               .find("[data-serverid='IngredientHeading']")
               .text()
-          : "ingredients";
-        ingredients[sectionName] = [];
+              .toLowerCase()}`
+          : "all";
+        recipe.ingredients[sectionName] = [];
 
         $(this)
           .find("li")
           .each(function(index, element) {
-            ingredients[sectionName].push($(this).text());
+            recipe.ingredients[sectionName].push($(this).text());
           });
       });
-
-      res.status(200).json(ingredients);
+      return recipe;
+    })
+    .then(createRecipeEntry)
+    .then(recipe => {
+      res.status(200).json(recipe);
     })
     .catch(err => {
       res.status(500).send(err);
+    });
+}
+
+function createRecipeEntry(recipe) {
+  return conn
+    .db()
+    .collection("recipes")
+    .insert(recipe)
+    .then(data => {
+      return data.ops[0];
     });
 }
